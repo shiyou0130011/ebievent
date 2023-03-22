04f046b9e9841df85ada8d832402bd313d754899
Tzuyang Tsai <14104502+shiyou0130011@users.noreply.github.com>
2023-03-22 10:16:12 +0800

    update game.Update() make it support mouse event

--- game.go
+++ game.go
@@ -1,8 +1,25 @@
 package ebievent
 
+import "github.com/shiyou0130011/go-event"
+
 type Game struct {
+	listeners       map[string][]event.Listener
+	isMouseParseing bool
 }
 
+type noneDispatchEvent string
+
+func (e noneDispatchEvent) Type() string {
+	return string(e)
+}
+
+// Update implements the ebiten.Game interface
 func (g *Game) Update() error {
+	if anyMouseButtonPressed() && !g.isMouseParseing {
+		g.DispatchEvent(noneDispatchEvent("click"))
+		g.isMouseParseing = true
+	} else if !anyMouseButtonPressed() {
+		g.isMouseParseing = false
+	}
 	return nil
 }
